39dbe08839868984fc6217d76426941a71619237
jwc20 <choij2015@gmail.com>
2022-06-24 12:44:23 -0700

    add

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # stitch_img_vertical
-# stitch_img_vertical
+
+## Usage
+
+Put multiple images into the directory with `vertical_img_combine.py` and run the python script.
+You can change the png to jpg or other image file types (I didn't try all of them).
+See example.
+
+## Installation
+
+To install, clone the repo.
+
+```
+git clone git@github.com:jwc20/stitch_img_vertical.git
+cd stitch_img_vertical
+pip install -r requirement.txt
+```
+
+### Requirements
+
+- Pillow
+- numpy
+
+Many thanks to [tutorial example](https://www.tutorialexample.com/combine-images-to-one-image-horizontally-and-vertically-using-python-pillow-a-step-guide-pillow-tutorial/)
---
+++ example/stitch_img_vertical.py
@@ -0,0 +1,52 @@
+import numpy as np
+from PIL import Image
+import glob
+
+list_im = glob.glob("*.png")
+total_width = 0
+total_height = 0
+max_width = 0
+max_height = 0
+min_width = 2000
+min_height = 0
+pre_w = 0
+pre_h = 0
+ix =[]
+
+for img in list_im:
+    im = Image.open(img)
+    size = im.size
+    w = size[0]
+    h = size[1]
+    total_width += w 
+    total_height += h
+
+    if h > max_height:
+        max_height = h
+    if w > max_width:
+        max_width = w
+    if h < min_height:
+        min_height = h
+    if w < min_width:
+        min_width = w
+    ix.append(im) 
+print("(total_width, total_height, max_width, max_height)")
+print((total_width, total_height, max_width, max_height))
+print("(min_width, max_width)")
+print((min_width, max_width))
+
+target_vertical = Image.new('RGB', (max_width, total_height))
+
+for img in ix:
+    target_vertical.paste(img, (pre_w, pre_h, pre_w+img.size[0], pre_h + img.size[1]))
+    pre_h += img.size[1]
+    
+    
+#target_vertical.show()
+#target_vertical.save('COMBINED.png', quality=100)
+
+tw, th = target_vertical.size
+target_vertical.crop((0, 0, tw-(max_width-min_width), th)).save('CROPPED.png', quality=100)
+
+
+
